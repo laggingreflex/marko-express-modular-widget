67fb27d9589c3d5bde6f6d448ab09bb8cc6039d7
Phillip Gates-Idem <phillip.idem@gmail.com>
2016-01-25 10:13:10 -0500

    Adding lasso-marko plugin and static route

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 
 const app = express();
 
+require('lasso').configure({
+    plugins: [
+        'lasso-marko'
+    ]
+});
+
 var marko = require('marko');
 var template = marko.load(require.resolve('./template.marko'));
-app.use(function(req, res) {
-  template.stream({}).pipe(res);
+
+app.get('/', function(req, res) {
+    template.stream({}).pipe(res);
 });
+
+app.use('/static', express.static(__dirname + '/static'));
 
 app.listen(8080, function() {
   console.log('Listening on localhost:8080');
